96d63084d8c9344a7433e3b9a51274189a78684a
keerthana <keerthananeeluru2001@gmail.com>
2023-09-06 10:22:29 +0530

    addining SimplonaTech

---
+++ SimplonaTech/SimplonaTech/Data/SimplonaTechDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SimplonaTech.Models;
+
+namespace SimplonaTech.Data
+{
+    public class SimplonaTechDbContext : DbContext
+    {
+        public SimplonaTechDbContext (DbContextOptions<SimplonaTechDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SimplonaTech.Models.DeptMaster> DeptMaster { get; set; } = default!;
+
+        public DbSet<SimplonaTech.Models.EmpProfile>? EmpProfile { get; set; }
+    }
+}
